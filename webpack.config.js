d109ba3be328a159027045de9403ddb77ce6337a
danielgynn <danielgynn@hotmail.co.uk>
2017-02-04 10:50:41 +0000

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+}
