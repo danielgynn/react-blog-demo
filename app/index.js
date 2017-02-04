f57f8de6b956d60fb80c53d8064bd9da2b06eae0
danielgynn <danielgynn@hotmail.co.uk>
2017-02-04 11:21:11 +0000

    Inclusde Sass loaders and import styles

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import SCSS from './styles/app.scss'
 
 ReactDOM.render(
   <App />, document.getElementById('container')
