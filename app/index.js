6b2eea16eb7a70173103595ffc01000a5061fbf6
danielgynn <danielgynn@hotmail.co.uk>
2017-02-04 11:15:51 +0000

    Install React and add App component

--- app/index.js
+++ app/index.js
@@ -1,1 +1,7 @@
-console.log('Running');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(
+  <App />, document.getElementById('container')
+);
